fd609ea6d1de0c08e3f3eb075a81b2000d2c44b2
HyejiYeom <kristi8041@gmail.com>
2023-02-09 22:05:39 +0900

    feat: Object collision implemented

--- js/scene/Mainscene.js
+++ js/scene/Mainscene.js
@@ -4,159 +4,178 @@ import Player from "../objects/Player.js";
 import Resource from "../objects/Resources.js";
 
 export default class MainScene extends Phaser.Scene {
-    constructor() {
-        super("MainScene");
-    }
+  constructor() {
+    super("MainScene");
+  }
 
-    init() {
-        // socket-io와 링크 스타~트!
-        this.socket = io();
+  init() {
+    // socket-io와 링크 스타~트!
+    this.socket = io();
 
-        this.playerId = null;
-        this.x = null;
-        this.y = null;
-        this.socket.on("start", (payLoad) => {
-            this.socketId = payLoad.socketId;
-            this.x = payLoad.x;
-            this.y = payLoad.y;
-        });
-    }
+    this.playerId = null;
+    this.x = null;
+    this.y = null;
+    this.socket.on("start", (payLoad) => {
+      this.socketId = payLoad.socketId;
+      this.x = payLoad.x;
+      this.y = payLoad.y;
+    });
+  }
 
-    preload() {
-        // 미리 로드하는 메서드, 이미지 등을 미리 로드한다.
-        Player.preload(this);
-        Resource.preload(this);
-        OtherPlayer.preload(this);
-        this.load.image("ground", "assets/images/ground.png");
-        this.load.tilemapTiledJSON("groundTile", "assets/images/ground.json");
-    }
-    create() {
-        // 생성해야 하는 것, 게임 오브젝트 등
+  preload() {
+    // 미리 로드하는 메서드, 이미지 등을 미리 로드한다.
+    Player.preload(this);
+    Resource.preload(this);
+    OtherPlayer.preload(this);
+    this.load.image("ground", "assets/images/ground.png");
+    // this.load.tilemapTiledJSON("groundTile", "assets/images/ground.json");
+    this.load.tilemapTiledJSON("groundTile", "assets/images/groundNew.json");
+    this.load.image("block1", "assets/images/block1.png");
+    this.load.image("block2", "assets/images/block2.png");
+    this.load.atlas(
+      "blocks",
+      "assets/images/resource.png",
+      "assets/images/resource.json"
+    );
+    this.load.json("blockss", "assets/images/resource.json");
+    // this.load.json("resources", "assets/resource.json");
+  }
+  create() {
+    // 생성해야 하는 것, 게임 오브젝트 등
 
-        this.map = this.make.tilemap({ key: "groundTile" });
-        const tileset = this.map.addTilesetImage("ground", "ground");
+    this.map = this.make.tilemap({ key: "groundTile" });
+    const tileset = this.map.addTilesetImage("ground", "ground");
 
-        const floorLayer = this.map.createStaticLayer(
-            "tilelayer",
-            tileset,
-            0,
-            0
-        );
-        floorLayer.setCullPadding(8, 8);
-        floorLayer.setCollisionByProperty({ collides: true });
-        this.matter.world.convertTilemapLayer(floorLayer);
+    const floorLayer = this.map.createStaticLayer("tilelayer", tileset, 0, 0);
+    floorLayer.setCullPadding(8, 8);
+    floorLayer.setCollisionByProperty({ collides: true });
+    this.matter.world.convertTilemapLayer(floorLayer);
 
-        // this.map.getObjectLayer("Resources").objects.forEach((resource) => {
-        //     new Resource({ scene: this, resource });
-        // });
+    const shapes = this.cache.json.get("blockss");
+    // this.map.getObjectLayer("obj").objects.forEach((resource) => {
+    //   new Resource({ scene: this, resource });
+    // });
 
-        this.player = new Player({
-            scene: this,
-            x: this.map.widthInPixels / 1.4,
-            y: this.map.heightInPixels / 3.5,
-            texture: "male1", // 이미지 이름
-            id: this.socketId,
-            frame: "down-1", // atlas.json의 첫번째 filename
-        });
-        this.player.inputKeys = this.input.keyboard.addKeys({
-            up: Phaser.Input.Keyboard.KeyCodes.W,
-            down: Phaser.Input.Keyboard.KeyCodes.S,
-            left: Phaser.Input.Keyboard.KeyCodes.A,
-            right: Phaser.Input.Keyboard.KeyCodes.D,
-        });
-        let camera = this.cameras.main;
-        camera.zoom = 0.8;
-        camera.startFollow(this.player);
-        camera.setLerp(0.1, 0.1);
-        // camera.setBounds(0, 0, this.game.config.width, this.game.config.height);
+    // var ground = this.matter.add.sprite(0, 0, "sheet", "ground", {
+    //   shape: shapes.ground,
+    // });
+    // ground.setPosition(0 + ground.centerOfMass.x, 280 + ground.centerOfMass.y);
 
-        createCharacterAnims(this.anims);
+    this.map.getObjectLayer("obj").objects.forEach((resource) => {
+      let resourceName = resource.name;
+      this.matter.add.sprite(resource.x, resource.y, resource.name, undefined, {
+        shape: shapes.block1,
+      });
+      //   this.matter.add.sprite(0, 0, "sheet", "ground", { shape: shapes.ground });
+    });
 
-        this.otherPlayers = [];
+    this.player = new Player({
+      scene: this,
+      x: this.map.widthInPixels / 1.4,
+      y: this.map.heightInPixels / 3.5,
+      texture: "male1", // 이미지 이름
+      id: this.socketId,
+      frame: "down-1", // atlas.json의 첫번째 filename
+    });
+    this.player.inputKeys = this.input.keyboard.addKeys({
+      up: Phaser.Input.Keyboard.KeyCodes.W,
+      down: Phaser.Input.Keyboard.KeyCodes.S,
+      left: Phaser.Input.Keyboard.KeyCodes.A,
+      right: Phaser.Input.Keyboard.KeyCodes.D,
+    });
+    let camera = this.cameras.main;
+    camera.zoom = 0.8;
+    camera.startFollow(this.player);
+    camera.setLerp(0.1, 0.1);
+    // camera.setBounds(0, 0, this.game.config.width, this.game.config.height);
 
-        this.socket.emit("currentPlayers");
+    createCharacterAnims(this.anims);
 
-        this.socket.on("currentPlayers", (payLoad) => {
-            const socketId = payLoad.socketId;
-            const x = payLoad.x;
-            const y = payLoad.y;
-            this.addOtherPlayers({ x: x, y: y, socketId: socketId });
-        });
+    this.otherPlayers = [];
 
-        this.socket.on("newPlayer", (payLoad) => {
-            this.addOtherPlayers({
-                x: payLoad.x,
-                y: payLoad.y,
-                socketId: payLoad.socketId,
-            });
-        });
+    this.socket.emit("currentPlayers");
 
-        this.socket.on("playerDisconnect", (socketId) => {
-            this.removePlayer(socketId);
-        });
+    this.socket.on("currentPlayers", (payLoad) => {
+      const socketId = payLoad.socketId;
+      const x = payLoad.x;
+      const y = payLoad.y;
+      this.addOtherPlayers({ x: x, y: y, socketId: socketId });
+    });
 
-        this.socket.on("updateLocation", (payLoad) => {
-            this.updateLocation(payLoad);
-        });
-    }
+    this.socket.on("newPlayer", (payLoad) => {
+      this.addOtherPlayers({
+        x: payLoad.x,
+        y: payLoad.y,
+        socketId: payLoad.socketId,
+      });
+    });
 
-    update() {
-        // if (!this.player) {
-        //     return;
-        // }
-        this.player.update();
-    }
+    this.socket.on("playerDisconnect", (socketId) => {
+      this.removePlayer(socketId);
+    });
 
-    addOtherPlayers(playerInfo) {
-        const otherPlayer = new OtherPlayer({
-            // playerInfo를 바탕으로 새로운 플레이어 객체를 그려준다.
-            // 해당 플레이어 객체를 움직이려면 어쩔까?
-            scene: this,
-            x: playerInfo.y,
-            y: playerInfo.x,
-            texture: "male1", // 이미지 이름
-            id: playerInfo.socketId,
-            frame: "down-1", // atlas.json의 첫번째 filename
-        });
-        otherPlayer.socketId = playerInfo.socketId;
-        this.otherPlayers.push(otherPlayer);
-    }
+    this.socket.on("updateLocation", (payLoad) => {
+      this.updateLocation(payLoad);
+    });
+  }
 
-    removePlayer(res) {
-        this.otherPlayers.forEach((player) => {
-            if (player.socketId === res) {
-                player.destroy();
-            }
-        });
-        this.otherPlayers.filter((player) => player.socketId !== res);
-    }
+  update() {
+    // if (!this.player) {
+    //     return;
+    // }
+    this.player.update();
+  }
 
-    updateLocation(payLoad) {
-        this.otherPlayers.forEach((player) => {
-            if (player.socketId === payLoad.socketId) {
-                switch (payLoad.motion) {
-                    case "left":
-                        player.play(`${player.playerTexture}-walk-left`, true);
-                        player.setPosition(payLoad.x, payLoad.y);
-                        break;
-                    case "right":
-                        player.play(`${player.playerTexture}-walk-right`, true);
-                        player.setPosition(payLoad.x, payLoad.y);
-                        break;
-                    case "up":
-                        player.play(`${player.playerTexture}-walk-up`, true);
-                        player.setPosition(payLoad.x, payLoad.y);
-                        break;
-                    case "down":
-                        player.play(`${player.playerTexture}-walk-down`, true);
-                        player.setPosition(payLoad.x, payLoad.y);
-                        break;
-                    case "idle":
-                        player.play(`${player.playerTexture}-idle-down`, true);
-                        player.setPosition(payLoad.x, payLoad.y);
-                        break;
-                }
-            }
-        });
-    }
+  addOtherPlayers(playerInfo) {
+    const otherPlayer = new OtherPlayer({
+      // playerInfo를 바탕으로 새로운 플레이어 객체를 그려준다.
+      // 해당 플레이어 객체를 움직이려면 어쩔까?
+      scene: this,
+      x: playerInfo.y,
+      y: playerInfo.x,
+      texture: "male1", // 이미지 이름
+      id: playerInfo.socketId,
+      frame: "down-1", // atlas.json의 첫번째 filename
+    });
+    otherPlayer.socketId = playerInfo.socketId;
+    this.otherPlayers.push(otherPlayer);
+  }
+
+  removePlayer(res) {
+    this.otherPlayers.forEach((player) => {
+      if (player.socketId === res) {
+        player.destroy();
+      }
+    });
+    this.otherPlayers.filter((player) => player.socketId !== res);
+  }
+
+  updateLocation(payLoad) {
+    this.otherPlayers.forEach((player) => {
+      if (player.socketId === payLoad.socketId) {
+        switch (payLoad.motion) {
+          case "left":
+            player.play(`${player.playerTexture}-walk-left`, true);
+            player.setPosition(payLoad.x, payLoad.y);
+            break;
+          case "right":
+            player.play(`${player.playerTexture}-walk-right`, true);
+            player.setPosition(payLoad.x, payLoad.y);
+            break;
+          case "up":
+            player.play(`${player.playerTexture}-walk-up`, true);
+            player.setPosition(payLoad.x, payLoad.y);
+            break;
+          case "down":
+            player.play(`${player.playerTexture}-walk-down`, true);
+            player.setPosition(payLoad.x, payLoad.y);
+            break;
+          case "idle":
+            player.play(`${player.playerTexture}-idle-down`, true);
+            player.setPosition(payLoad.x, payLoad.y);
+            break;
+        }
+      }
+    });
+  }
 }
--- server.js
+++ server.js
@@ -13,81 +13,81 @@ app.get("/", (req, res) => res.sendFile(__dirname + "/index.html"));
 // Store a list of all the players
 let players = [];
 
-httpServer.listen(5000);
+httpServer.listen(3000);
 
 io.on("connection", (socket) => {
-    // socket이 연결됩니다~ 이 안에서 서버는 연결된 클라이언트와 소통할 준비가 됨
-    console.log("a user connected");
-    console.log(players.length);
-    let playerInfo = {
-        socket: socket,
-        socketId: socket.id,
-        x: 200,
-        y: 200,
-    };
-    // The payload to be sent back to the client
+  // socket이 연결됩니다~ 이 안에서 서버는 연결된 클라이언트와 소통할 준비가 됨
+  console.log("a user connected");
+  console.log(players.length);
+  let playerInfo = {
+    socket: socket,
+    socketId: socket.id,
+    x: 200,
+    y: 200,
+  };
+  // The payload to be sent back to the client
+  const payLoad = {
+    socketId: socket.id,
+    x: 200,
+    y: 200,
+  };
+  // Send back the payload to the client and set its initial position
+  socket.emit("start", payLoad);
+  // Send back the payload to the client and set its initial position
+  players.forEach((player) => {
     const payLoad = {
-        socketId: socket.id,
-        x: 200,
-        y: 200,
+      socketId: socket.id,
+      x: player.x,
+      y: player.y,
     };
-    // Send back the payload to the client and set its initial position
-    socket.emit("start", payLoad);
-    // Send back the payload to the client and set its initial position
+    player.socket.emit("newPlayer", payLoad);
+  });
+
+  players.push(playerInfo);
+
+  socket.on("disconnect", () => {
+    // socket이 연결 해제됩니다~
+    console.log("user disconnected!!!");
+    socket.broadcast.emit("playerDisconnect", socket.id);
+    // players.forEach((player) => {
+    //     if (player.socketId !== socket.id) {
+    //         player.socket.emit("playerDisconnect", socket.id);
+    //     }
+    // });
+    players = players.filter((player) => player.socketId !== socket.id);
+  });
+
+  socket.on("currentPlayers", () => {
     players.forEach((player) => {
+      if (player.socketId !== socket.id) {
         const payLoad = {
-            socketId: socket.id,
-            x: player.x,
-            y: player.y,
+          socketId: player.socketId,
+          x: player.x,
+          y: player.y,
         };
-        player.socket.emit("newPlayer", payLoad);
+        socket.emit("currentPlayers", payLoad);
+      }
     });
+  });
 
-    players.push(playerInfo);
+  socket.on("movement", (xy) => {
+    const payLoad = {
+      socketId: socket.id,
+      x: xy.x,
+      y: xy.y,
+      motion: xy.motion,
+    };
+    socket.broadcast.emit("updateLocation", payLoad);
+    playerInfo.x = xy.x;
+    playerInfo.y = xy.y;
+    // players.forEach((player) => {
+    //     if (player.socketId !== socket.id) {
 
-    socket.on("disconnect", () => {
-        // socket이 연결 해제됩니다~
-        console.log("user disconnected!!!");
-        socket.broadcast.emit("playerDisconnect", socket.id);
-        // players.forEach((player) => {
-        //     if (player.socketId !== socket.id) {
-        //         player.socket.emit("playerDisconnect", socket.id);
-        //     }
-        // });
-        players = players.filter((player) => player.socketId !== socket.id);
-    });
-
-    socket.on("currentPlayers", () => {
-        players.forEach((player) => {
-            if (player.socketId !== socket.id) {
-                const payLoad = {
-                    socketId: player.socketId,
-                    x: player.x,
-                    y: player.y,
-                };
-                socket.emit("currentPlayers", payLoad);
-            }
-        });
-    });
-
-    socket.on("movement", (xy) => {
-        const payLoad = {
-            socketId: socket.id,
-            x: xy.x,
-            y: xy.y,
-            motion: xy.motion,
-        };
-        socket.broadcast.emit("updateLocation", payLoad);
-        playerInfo.x = xy.x;
-        playerInfo.y = xy.y;
-        // players.forEach((player) => {
-        //     if (player.socketId !== socket.id) {
-
-        //         player.socket.emit("updateLocation", payLoad);
-        //     } else {
-        //         player.x = xy.x;
-        //         player.y = xy.y;
-        //     }
-        // });
-    });
+    //         player.socket.emit("updateLocation", payLoad);
+    //     } else {
+    //         player.x = xy.x;
+    //         player.y = xy.y;
+    //     }
+    // });
+  });
 });
